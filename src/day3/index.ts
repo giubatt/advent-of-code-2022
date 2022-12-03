e5dae031415b25a32e10b9aeec300e37d2822a0f
Giuseppe Battistella <bartgb00@gmail.com>
2022-12-03 10:36:45 -0300

    add day3

---
+++ src/day3/index.ts
@@ -0,0 +1,59 @@
+import {
+  dirname,
+  fromFileUrl,
+  resolve,
+} from "https://deno.land/std@0.167.0/path/mod.ts";
+import { sum } from "../helpers.ts";
+import { match } from "npm:ts-pattern";
+import { assert } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import { chunk } from "https://deno.land/std@0.167.0/collections/chunk.ts";
+
+const input = await Deno.readTextFile(
+  resolve(dirname(fromFileUrl(import.meta.url)), "./input.txt"),
+);
+
+const parseRucksacks = (input: string) => {
+  const result = input.split("\n").map((
+    row,
+  ) => ([row.slice(0, row.length / 2), row.slice(row.length / 2)] as const));
+  return result.slice(0, result.length - 1);
+};
+
+const getItemPriority = (item: string) => {
+  return match(item[0])
+    .when((char) => char.match(/[a-z]/), (char) => char.charCodeAt(0) - 96)
+    .when((char) => char.match(/[A-Z]/), (char) => char.charCodeAt(0) - 38)
+    .otherwise(() => 0);
+};
+
+const rucksacks = parseRucksacks(input);
+
+const misplacedItems = rucksacks.map(([first, second]) => {
+  let repeatedItem = "";
+
+  first.split("").forEach((item) => {
+    if (second.includes(item)) {
+      repeatedItem = item;
+    }
+  });
+
+  return repeatedItem;
+});
+
+// asnwer 1
+assert(sum(misplacedItems.map(getItemPriority)) === 8088);
+
+const elfGroups = chunk(
+  rucksacks.map(([first, second]) => first.concat(second)),
+  3,
+);
+
+const getGroupBadge = (group: string[]) => {
+  const repeatedItem = group[0].split("").find((item) => {
+    return group[1].includes(item) && group[2].includes(item);
+  });
+  return repeatedItem || "";
+};
+
+// asnwer 2
+assert(sum(elfGroups.map(getGroupBadge).map(getItemPriority)) === 2522);
